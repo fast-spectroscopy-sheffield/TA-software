650bfaaf3de5a784e8ad7efa36c5be7d3dd75d8c
davidbossanyi <davebossanyi@gmail.com>
2019-02-19 17:41:39 +0000

    Update visTA.bat

--- visTA_v1.01/visTA.bat
+++ visTA_v1.01/visTA.bat
@@ -1,6 +1,6 @@
 call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
 call cd C:\Users\Lab_Test
-call conda activate pyTAtest
-call cd Documents\TASoftware\visTA_v1.0
+call conda activate pyTA
+call cd Documents\TASoftware\visTA_v1.01
 call python visTA.py
 call cd C:\Users\Lab_Test
