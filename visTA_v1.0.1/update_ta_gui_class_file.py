550a6e4555bad5535e0cd96d929c335550b1e579
davidbossanyi <davebossanyi@gmail.com>
2019-02-20 10:21:38 +0000

    remove absolute path in update_ta_gui_class_file

--- visTA_v1.0.1/update_ta_gui_class_file.py
+++ visTA_v1.0.1/update_ta_gui_class_file.py
@@ -3,7 +3,7 @@
 import PyQt5.uic
 import os
 
-fpath = os.path.join(os.path.expanduser('~'), 'Documents', 'GitHub', 'TA-software', 'visTA_v1.01', 'ta_gui.ui')
+fpath = 'ta_gui.ui'
 
 with open('ta_gui_class.py','w') as file:
     PyQt5.uic.compileUi(fpath, file)
